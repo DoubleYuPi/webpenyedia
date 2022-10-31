fe3673639f1b73eaca9b0fcbcbee6778c2d58f25
William Phedra <williamphedra@gmail.com>
2022-10-31 14:00:41 +0700

    Added section class

--- resources/views/admin/detailpenyedia.blade.php
+++ resources/views/admin/detailpenyedia.blade.php
@@ -1,5 +1,53 @@
 <section class="content">
-    
-</section>
+  <div class="container-fluid">
+    <div class="row">
+      <!-- left column -->
+      <div class="col-12">
+        <!-- general form elements -->
+        <div class="card card-primary">
+          <!-- /.card-header -->
+          <!-- form start -->
+              <div class="card-body">
+                
+              </div>
+            <!-- /.card-body -->
+          
+        </div>
+      </div>
+  </section>
 
+  <section class="content">
+    <div class="container-fluid">
+      <div class="row">
+        <!-- left column -->
+        <div class="col-12">
+          <!-- general form elements -->
+          <div class="card card-primary">
+            <!-- /.card-header -->
+            <!-- form start -->
+                <div class="card-body">
+                  
+                </div>
+              <!-- /.card-body -->
+            
+          </div>
+        </div>
+    </section>
 
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <!-- left column -->
+          <div class="col-12">
+            <!-- general form elements -->
+            <div class="card card-primary">
+              <!-- /.card-header -->
+              <!-- form start -->
+                  <div class="card-body">
+                    
+                  </div>
+                <!-- /.card-body -->
+              
+            </div>
+          </div>
+      </section>
