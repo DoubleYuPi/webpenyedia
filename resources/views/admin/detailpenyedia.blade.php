2c8d16ce44ffa30eaa9341446c02d132b800acd5
William Phedra <williamphedra@gmail.com>
2022-11-01 15:37:12 +0700

    Added content npwp and alamat

--- resources/views/admin/detailpenyedia.blade.php
+++ resources/views/admin/detailpenyedia.blade.php
@@ -1,19 +1,19 @@
 <section class="content">
   <div class="container-fluid">
-    <div class="row">
-      <!-- left column -->
-      <div class="col-12">
-        <!-- general form elements -->
-        <div class="card card-primary">
-          <!-- /.card-header -->
-          <!-- form start -->
-              <div class="card-body">
-                
-              </div>
-            <!-- /.card-body -->
-          
+    <div class="card card-default color-palette-box">
+      <div class="card-header">
+        <h3 class="card-title">
+          <b>{{$penyedia->nama}}</b>
+        </h3>
+      </div>
+      <div class="card-body">
+        <div class="col-12">
+          <h5><i class="fas fa-id-card"></i>&nbsp NPWP: {{$penyedia->npwp}}</h5><br>
+          <h5><i class="fas fa-map-pin"></i>&nbsp Alamat: {{$penyedia->alamat}}</h5>
         </div>
       </div>
+    </div>
+  </div>
   </section>
 
   <section class="content">
